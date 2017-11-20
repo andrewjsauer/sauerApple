b5ee9362c7a0d7c825c1f99f77c5c07c8a2e8eb1
andrewjsauer <andrewjsauer@users.noreply.github.com>
2017-11-19 19:12:14 -0800

    take 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,25 +99,25 @@
 
 		})();
 
-		$.ajax({
-			url: '//formspree.io/gosauerapple@gmail.com',
-			method: 'POST',
-			data: $(this).serialize(),
-			dataType: 'json',
-			beforeSend: function() {
-				$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
-			},
-
-			success: function(data) {
-				$contactForm.find('.alert--loading').hide();
-				$contactForm.append('<div class="alert alert--success">Message sent!</div>');
-			},
-
-			error: function(err) {
-				$contactForm.find('.alert--loading').hide();
-				$contactForm.append('<div class="alert alert--error">Oops, error.</div>');
-			}
-		});
+		// ajax({
+		// 	url: '//formspree.io/gosauerapple@gmail.com',
+		// 	method: 'POST',
+		// 	data: $(this).serialize(),
+		// 	dataType: 'json',
+		// 	beforeSend: function() {
+		// 		$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+		// 	},
+		//
+		// 	success: function(data) {
+		// 		$contactForm.find('.alert--loading').hide();
+		// 		$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+		// 	},
+		//
+		// 	error: function(err) {
+		// 		$contactForm.find('.alert--loading').hide();
+		// 		$contactForm.append('<div class="alert alert--error">Oops, error.</div>');
+		// 	}
+		// });
 
 	// Signup Form.
 		// (function() {
